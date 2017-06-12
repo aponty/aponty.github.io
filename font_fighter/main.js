20ddd83f38f3021e0715fec3f640ae9813ff2c93
Anson <aponty@gmail.com>
2017-06-12 16:23:47 -0400

    spin by level, better brick collision

--- font_fighter/main.js
+++ font_fighter/main.js
@@ -44,8 +44,9 @@ $(function() {
   var $livesDisplay = $('span').eq(1);
   var timesFourForBrickNumber = 5;
   var currentLevel = 1;
-  var infiniteLevelSetBoardInt; //if named in one function, can't cancle from another; hence declared here
-  var input; //likewise declared here to make accessible across functions
+  var infiniteLevelSetBoardInt;
+  var input;
+  var spin = .75;
 
   $('input').keydown(function(x) {
     if (x.which === 13) {
@@ -121,21 +122,25 @@ $(function() {
       timesFourForBrickNumber = 1;
       paddleVelocity = 7;
       lives = 4;
+      spin = .9
     }
     if (currentLevel === 2) {
       timesFourForBrickNumber = 3;
       paddleVelocity = 6;
       lives = 3;
+      spin = .8
     }
     if (currentLevel === 3) {
       timesFourForBrickNumber = 10;
       paddleVelocity = 8;
       lives = 2;
+      spin = .7
     }
     if (currentLevel === '∞') {
       timesFourForBrickNumber = 2;
       paddleVelocity = 8;
       lives = 3;
+      spin = .6
       infiniteLevelSetBoardInt = setInterval(function() {
         setBoard();
       }, 8000);
@@ -218,30 +223,30 @@ $(function() {
   };
 
   function spinCheck() {
-    var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(.75 * ballVertVelocity, 2)) / Math.pow(ballHorVelocity, 2));
-    var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(.75 * ballHorVelocity, 2)) / Math.pow(ballVertVelocity, 2));
+    var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(spin * ballVertVelocity, 2)) / Math.pow(ballHorVelocity, 2));
+    var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(spin * ballHorVelocity, 2)) / Math.pow(ballVertVelocity, 2));
 
     //if paddle right and ball right
     if (keys[39] && paddleLeft < 675 && ballHorVelocity > 0) {
-      ballVertVelocity *= .75;
+      ballVertVelocity *= spin;
       ballHorVelocity *= z;
       console.log('spun flat');
     }
     //if paddle right and ball left
     if (keys[39] && paddleLeft < 675 && ballHorVelocity < 0) {
       ballVertVelocity *= m;
-      ballHorVelocity *= .75;
+      ballHorVelocity *= spin;
       console.log('spun up');
     }
     //if paddle left and ball right
     if (keys[37] && paddleLeft >= 1 && ballHorVelocity > 0) {
       ballVertVelocity *= m;
-      ballHorVelocity *= .75;
+      ballHorVelocity *= spin;
       console.log('spun up');
     }
     //if paddle left and ball left
     if (keys[37] && paddleLeft >= 1 && ballHorVelocity < 0) {
-      ballVertVelocity *= .75;
+      ballVertVelocity *= spin;
       ballHorVelocity *= z;
       console.log('spun flat');
     }
@@ -260,32 +265,24 @@ $(function() {
     };
   };
 
-  function ballBrickCollisionCheck() { //need to keep working on this
+  function ballBrickCollisionCheck() {
     document.querySelectorAll('.brick').forEach(x => {
       var top = parseFloat(x.style.top.split('px')[0]);
       var left = parseFloat(x.style.left.split('px')[0]);
-      var right = widthByClass[x.classList[1]];
-      //left edge. Sequence of elses is arbitrary; leaving them as naked ifs led to multiple delete attempts on some corner strikes
-      if (ballHorPos >= left && ballHorPos <= (left + 10) && ballVertPos >= top && ballVertPos <= (top + 50)) {
-        x.parentNode.removeChild(x);
-        score += 5;
-        ballHorVelocity = -ballHorVelocity;
-      } else //bottom edge. Need to pick edges specifically to bounce horizontal vs vert. === to bottom edge gets jumped over. Added 10px fuzz
-        if (ballVertPos <= (top + 50) && ballVertPos >= (top + 40) && ballHorPos >= left && ballHorPos <= (left + right)) {
+      var width = widthByClass[x.classList[1]];
+      var ballHC = ballHorPos + 5; //horizontal center
+
+      if (ballVertPos <= (top + 50) && ballVertPos >= top && ballHorPos >= left && ballHorPos <= (left + width)) { //if there's any overlap
+        if (ballHC > left && ballHC < left + width) {  //if the ball's center is not within the horizonal bounds, it's coming from top or bottom
           x.parentNode.removeChild(x);
           score += 5;
-          ballVertVelocity = -ballVertVelocity;
-        } else //right edge
-          if (ballHorPos <= (left + right) && ballHorPos >= (left + right - 10) && ballVertPos >= top && ballVertPos <= (top + 50)) {
-            x.parentNode.removeChild(x);
-            score += 5;
-            ballHorVelocity = -ballHorVelocity;
-          } else //top edge
-            if (ballVertPos >= top && ballVertPos <= (top + 10) && ballHorPos >= left && ballHorPos <= (left + right)) {
-              x.parentNode.removeChild(x);
-              score += 5;
-              ballVertVelocity = -ballVertVelocity;
-            }
+          ballVertVelocity = -ballVertVelocity; //so vertical bounce
+        } else {
+          x.parentNode.removeChild(x);
+          score += 5;
+          ballHorVelocity = -ballHorVelocity;  // else horizontal bounce. Still a bit of a random prioritization in cases of corner strikes
+        }
+      }
     })
   };
 
