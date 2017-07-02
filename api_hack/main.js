e000bf9bc5141ab78f0399becc6be1880581f025
Anson <aponty@gmail.com>
2017-07-02 17:06:18 -0400

    ssd

--- api_hack/main.js
+++ api_hack/main.js
@@ -5,7 +5,8 @@ for (let i = 0; i < 7; i++) {
     arr.push(Math.floor(Math.random() * 6))
 }
 
-const y = document.createTextNode(arr)
-
+const y = document.createElement('div')
+y.textContent = arr
+y.setAttribute('data', arr)
 body.appendChild(y)
 console.log(arr)
