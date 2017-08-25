cccb326650e62f0ad44e6c72539c5ecc6e797817
Anson <aponty@gmail.com>
2017-08-25 16:26:43 -0400

    mobile improved, still have nav kinks

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 "use strict";
 window.onload = () => {
-    //prevents skrollr from jumping to leftover hash in url on refresh
-    document.location.hash = null;
-
     const scroll = skrollr.init({
         constants: {
             box: '100p'
         },
-        //for debugging
+
         render: function(data) {
             console.log(data.curTop)
-        }
+        },
+
     });
 
-    skrollr.menu.init(scroll)
+    skrollr.menu.init(scroll, {
+        updateUrl: false
+    })
 
 
 
